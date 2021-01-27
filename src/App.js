0f0a13b398f06543b37084cd930b2f5ca36fe660
Maria Elisa Araya <arayamariaelisa@gmail.com>
2021-01-27 07:55:23 -0300

    se agrega el contenedor en app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 //importar el nombre de nuestro componente from -posicion-
 import './App.css';
-import NavBar from './components/NavBar';
+import NavBar from './components/NavBar/index';
+import ItemListContainer from './components/ItemListContainer/index'
 
 function App() {//app es el padre de header
   return (
     <>
       <NavBar /> {/*llamo a ese componente que dentro tiene mini componentes, html -> jsx mayusc*/}
+      <ItemListContainer /> 
     </>
   );
 }
