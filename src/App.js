f395f2e0d39d47e061a1c58920f3fdb2add7b590
Maria Elisa Araya <arayamariaelisa@gmail.com>
2021-01-27 06:37:33 -0300

    se agrega navbar en app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import Header from './components/header' //importar el nombre de nuestro componente from -posicion-
+//importar el nombre de nuestro componente from -posicion-
 import './App.css';
+import NavBar from './components/NavBar';
 
 function App() {//app es el padre de header
   return (
     <>
-      <Header /> {/*llamo a ese componente que dentro tiene mini componentes, html -> jsx mayusc*/}
+      <NavBar /> {/*llamo a ese componente que dentro tiene mini componentes, html -> jsx mayusc*/}
     </>
   );
 }
